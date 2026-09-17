00ae0801e1267bf4169e5d465f742185b4b42585
abed-sketch <abed@apex-consulting.ai>
2026-09-17 20:34:49 +0300

    feat: add German-English portal switch and verified demo data handoff (#6)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { LanguageProvider } from "./i18n";
 import { App } from "./App";
 import { consumeToken } from "./api/token";
 import "./styles.css";
@@ -8,6 +9,6 @@ const token = consumeToken(new URL(window.location.href), (path) =>
 );
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App token={token} />
+    <LanguageProvider><App token={token} /></LanguageProvider>
   </React.StrictMode>,
 );
